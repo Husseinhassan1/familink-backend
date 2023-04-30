b7dc1d6cf275714e3f1313073e92a1d364c6ebbf
BiggestM <muhamed.mehmedovic@stu.ssst.edu.ba>
2023-05-01 01:55:00 +0200

    Changed UserEntity so email and username are unique

--- src/main/java/com/familink/backend/models/entities/UserEntity.java
+++ src/main/java/com/familink/backend/models/entities/UserEntity.java
@@ -10,7 +10,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity
-@Table(name = "app_user")
+@Table(name = "app_user", uniqueConstraints = @UniqueConstraint(columnNames = {"email", "username"}))
 @Getter
 @Setter
 @AllArgsConstructor
