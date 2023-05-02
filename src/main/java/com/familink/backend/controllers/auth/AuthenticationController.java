88a0cd2d1d36819158964c99092beafe821a2b5b
BiggestM <muhamed.mehmedovic@stu.ssst.edu.ba>
2023-05-02 03:09:32 +0200

    fixed

--- src/main/java/com/familink/backend/controllers/auth/AuthenticationController.java
+++ src/main/java/com/familink/backend/controllers/auth/AuthenticationController.java
@@ -4,14 +4,12 @@ package com.familink.backend.controllers.auth;
 import com.familink.backend.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins="http://localhost:4200/")
 public class AuthenticationController {
 
     private final AuthenticationService service;
